7880dcfc58f026dd7e505fa469afd7836f140efb
aliceni <aliceni@bu.edu>
2024-04-25 20:43:19 -0400

    made edits to submit forecast function

--- 06_uncertainty_partitioning.R
+++ 06_uncertainty_partitioning.R
@@ -58,13 +58,13 @@ N.IPD <- forecast(IC = IC[prow, 30],
 N.IPD.ci = apply(N.IPD,2,quantile,c(0.025,0.5,0.975), na.rm=TRUE)
 
 # Process uncertainty
-N.IPDE <- forecast(IC=IC[prow, 30],
-                   temp = temps_rot[drow,],
-                   betaTemp = params[prow, "betaTemperature"],
-                   betaX = params[prow, "betaX"],
-                   betaI = params[prow, "betaIntercept"],
-                   Q = Qmc,
-                   n = Nmc)
+N.IPDE <- pheno_forecast#forecast(IC=IC[prow, 30],
+          #         temp = temps_rot[drow,],
+          #         betaTemp = params[prow, "betaTemperature"],
+          #         betaX = params[prow, "betaX"],
+          #         betaI = params[prow, "betaIntercept"],
+          #         Q = Qmc,
+          #         n = Nmc)
 N.IPDE.ci = apply(N.IPDE,2,quantile,c(0.025,0.5,0.975), na.rm=TRUE)
 
 ## Plot run with all the uncertainty partitioned- see corresponding RMD file for step-by-step
@@ -73,7 +73,39 @@ ecoforecastR::ciEnvelope(time2,N.IPDE.ci[1,],N.IPDE.ci[3,],col="darkgoldenrod1")
 ecoforecastR::ciEnvelope(time2,N.IPD.ci[1,],N.IPD.ci[3,],col="orchid")
 ecoforecastR::ciEnvelope(time2,N.IP.ci[1,],N.IP.ci[3,],col="pink")
 ecoforecastR::ciEnvelope(time2, N.I.ci[1,], N.I.ci[3,], col="lightgreen")
-legend("topleft", inset=0.01, legend = c("Initial Condition", "Parameter", "Driver", "Process"), 
+legend("topleft", legend = c("Initial Condition", "Parameter", "Driver", "Process"), 
        col = c("lightgreen","pink","orchid","darkgoldenrod1"), lty=1, lwd=5)
 lines(time2,N.det_pheno,lwd=1)
+
+# Uncertainty Analysis
+
+### calculation of variances
+varI     <- apply(N.I,2,var)
+varIP    <- apply(N.IP,2,var)
+varIPD   <- apply(N.IPD,2,var)
+varIPDE  <- apply(N.IPDE,2,var)
+#varIPDEA <- apply(N.IPDEA,2,var)
+varMat   <- rbind(varI,varIP,varIPD,varIPDE)#,varIPDEA)
+
+## out-of-sample stacked area plot
+V.pred.rel <- apply(varMat,2,function(x) {x/max(x)})
+plot(time2,V.pred.rel[1,],ylim=c(0,1),type='n',main="Relative Variance: Out-of-Sample",ylab="Proportion of Variance",xlab="time")
+ciEnvelope(time2,rep(0,ncol(V.pred.rel)),V.pred.rel[1,],col="lightgreen")
+ciEnvelope(time2,V.pred.rel[1,],V.pred.rel[2,],col="pink")
+ciEnvelope(time2,V.pred.rel[2,],V.pred.rel[3,],col="orchid")
+ciEnvelope(time2,V.pred.rel[3,],V.pred.rel[4,],col="darkgoldenrod1")
+#ciEnvelope(time2,V.pred.rel[4,],V.pred.rel[5,],col=N.cols[5])
+legend("topleft",legend=c("Process","Driver","Parameter","InitCond"),
+       col=c("lightgreen","pink","orchid","darkgoldenrod1"),
+       lty=1,lwd=5)
+
+## in-sample stacked area plot
+V.pred.rel.in <- apply(varMat[-5,],2,function(x) {x/max(x)})
+plot(time2,V.pred.rel.in[1,],ylim=c(0,1),type='n',main="Relative Variance: In-Sample",ylab="Proportion of Variance",xlab="time")
+ciEnvelope(time2,rep(0,ncol(V.pred.rel.in)),V.pred.rel.in[1,],col="lightgreen")
+ciEnvelope(time2,V.pred.rel.in[1,],V.pred.rel.in[2,],col="pink")
+ciEnvelope(time2,V.pred.rel.in[2,],V.pred.rel.in[3,],col="orchid")
+ciEnvelope(time2,V.pred.rel.in[3,],V.pred.rel.in[4,],col="darkgoldenrod1")
+legend("topleft",legend=c("Process","Driver","Parameter","InitCond"),
+       col=c("lightgreen","pink","orchid","darkgoldenrod1"),lty=1,lwd=5)
 
